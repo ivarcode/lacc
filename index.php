e3208312533e777a0b7831250d9a86270dbc851a
Camden Wagner <camden.i.wagner@ivarcode.net>
2017-06-27 00:11:01 -0700

    basic layout

---
+++ index.php
@@ -0,0 +1,55 @@
+<!--
+Author: Camden I. Wagner
+File: index.php
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Website Title & Description -->
+	<title>LA Chess Club</title>
+	<meta name="description" content="">
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="styles.css">
+
+	<!-- Custom JS -->
+	<script type="text/javascript">
+
+	</script>
+
+</head>
+<body>
+
+	<section id="header" name="header">
+		<div class="row">
+			<div class="col-2"></div>
+			<div class="col-1">
+				<center><img style="margin-top: 10px;-moz-transform: scaleX(-1);-o-transform: scaleX(-1);-webkit-transform: scaleX(-1);transform: scaleX(-1);filter: FlipH;-ms-filter: "FlipH";" src="img/pieces/w_Knight.png"></center>
+			</div>
+			<div class="col-6">
+				<center><h1 id="title">Los Angeles Chess Club</h1></center>
+			</div>
+			<div class="col-1">
+				<center><img style="margin-top: 10px;" src="img/pieces/b_Knight.png"></center>
+			</div>
+			<div class="col-2"></div>
+		</div>
+	</section>
+
+	<section id="nav" name="nav">
+		<div class="row">
+			<div class="col-2"></div>
+			<div class="col-8" style="background: rgba(0,0,0,0.7);">
+				text
+			</div>
+			<div class="col-2"></div>
+		</div>
+	</section>
+
+	<section id="main" name="main">
+
+	</section>
+
+</body>
+</html>
